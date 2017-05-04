d896c6cf1d11775f38c8075cf420590213bcbbb4
Paolo Rascuna <prascuna@mdsol.com>
2017-05-04 14:51:57 +0100

    Pimp my library! Configuraiton has been pimped

--- 02-pimp-my-library/src/main/scala/replacer/config/AppConfig.scala
+++ 02-pimp-my-library/src/main/scala/replacer/config/AppConfig.scala
@@ -5,25 +5,38 @@ import com.typesafe.config.Config
 
 class AppConfig(config: Config) {
 
+  import AppConfig._
+
   val serverConfig = ServerConfig(
     host = config.getString("server.host"),
     port = config.getInt("server.port")
   )
 
   val replacerConfig = ReplacerConfig(
-    uri = Uri(config.getString("replacer.uri")),
-    method = config.getString("replacer.method") match {
-      case "CONNECT" => HttpMethods.CONNECT
-      case "DELETE" => HttpMethods.DELETE
-      case "GET" => HttpMethods.GET
-      case "HEAD" => HttpMethods.HEAD
-      case "OPTIONS" => HttpMethods.OPTIONS
-      case "PATCH" => HttpMethods.PATCH
-      case "POST" => HttpMethods.POST
-      case "PUT" => HttpMethods.PUT
-      case "TRACE" => HttpMethods.TRACE
-    }
+    uri = config.getURI("replacer.uri"),
+    method = config.getHttpMethod("replacer.method")
   )
+
+}
+
+object AppConfig {
+
+  implicit class RichConfig(config: Config) {
+    def getURI(path: String): Uri = Uri(config.getString(path))
+
+    def getHttpMethod(path: String): HttpMethod =
+      config.getString(path) match {
+        case "CONNECT" => HttpMethods.CONNECT
+        case "DELETE" => HttpMethods.DELETE
+        case "GET" => HttpMethods.GET
+        case "HEAD" => HttpMethods.HEAD
+        case "OPTIONS" => HttpMethods.OPTIONS
+        case "PATCH" => HttpMethods.PATCH
+        case "POST" => HttpMethods.POST
+        case "PUT" => HttpMethods.PUT
+        case "TRACE" => HttpMethods.TRACE
+      }
+  }
 
 }
 
