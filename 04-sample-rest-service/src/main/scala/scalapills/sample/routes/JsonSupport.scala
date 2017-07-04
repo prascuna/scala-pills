a1784163abef507e062cce1b8895dbe01d209269
Paolo Rascuna <prascuna@mdsol.com>
2017-07-04 15:44:08 +0100

    dropping null keys for json

--- 04-sample-rest-service/src/main/scala/scalapills/sample/routes/JsonSupport.scala
+++ 04-sample-rest-service/src/main/scala/scalapills/sample/routes/JsonSupport.scala
@@ -2,8 +2,10 @@ package scalapills.sample.routes
 
 
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import io.circe.Printer
 import io.circe.generic.AutoDerivation
 
 trait JsonSupport extends FailFastCirceSupport with AutoDerivation {
-
+  // Uncomment this if you want the options = None to drop the key
+  // implicit val printer = Printer.noSpaces.copy(dropNullKeys = true)
 }
